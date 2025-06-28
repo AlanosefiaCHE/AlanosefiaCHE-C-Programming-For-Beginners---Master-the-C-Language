5ad488fb273c7ba038c7840bdea7d3759ae7f7b5
Alan Osefia <amosefia@student.che.nl>
2025-06-29 00:41:10 +0200

    Challenge section 4 optimalization

--- Section4Challenge/main.c
+++ Section4Challenge/main.c
@@ -4,23 +4,15 @@ int main(int argc, char **argv)
 {
 	enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
 
-
-	typedef struct myCompanies {
-	enum Company Google;
-	enum Company Xerox;
-	enum Company Ebay;
+	struct myCompanies {
+	enum Company CompanyName[3];
 		};
 		
-	struct myCompanies structCompanies;
+	struct myCompanies mcName = {{XEROX,GOOGLE,EBAY}};
 	
-	structCompanies.Google = GOOGLE;
-	structCompanies.Xerox= XEROX;
-	structCompanies.Ebay = EBAY;
-
-	
-	printf("Xerox number is: %d\n", structCompanies.Xerox);
-	printf("Google number is: %d\n", structCompanies.Google);
-	printf("Ebay number is: %d\n", structCompanies.Ebay);
+	printf("Xerox number in array is: %d\n", mcName.CompanyName[0]);
+	printf("Google number in array is: %d\n", mcName.CompanyName[1]);
+	printf("Ebay number in array is: %d\n", mcName.CompanyName[2]);
 
 	return 0;
 }
